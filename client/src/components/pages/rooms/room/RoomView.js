d84dc9f9ffea3900e048de52cf4ace147c993132
Marjut <marjut.kinnari@gmail.com>
2020-03-06 11:02:58 +0200

    last changes

--- client/src/components/pages/rooms/room/RoomView.js
+++ client/src/components/pages/rooms/room/RoomView.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from 'react';
 import io from "socket.io-client";
 import RoomHeader from './RoomHeader';
 import RoomMessageArea from './RoomMessageArea';
-
+import RoomMessage from './RoomMessage';
 
 const RoomView = (props) => {
 
 
   return (
-    <div>
+    <div className="">
       <RoomHeader match={props.match} history={props.history}/>
+      <RoomMessage />
       <RoomMessageArea />
     </div>
   );
